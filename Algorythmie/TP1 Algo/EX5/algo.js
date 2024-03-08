78fe575d0e6e1718dee06b5073b423b5c3ef5f3f
Damien <damien.tisne@free.fr>
2024-03-08 11:03:04 +0100

    Ajout travail 08/03 part 1

---
+++ Algorythmie/TP1 Algo/EX5/algo.js
@@ -0,0 +1,9 @@
+let nb1 =parseInt (prompt("Valeur 1"))
+let nb2 =parseInt (prompt("Valeur 2"))
+
+add = nb1 + nb2
+sous = nb1 - nb2
+mult = nb1 * nb2
+divi = nb1 / nb2
+
+alert("Addition : " + add + " ; Soustraction : " + sous + " ; Multiplication : " + mult + )
