3884a168047afa1b123b88a75f3abed4b809014c
Damien Tisné <damien.tisne@free.fr>
2024-03-20 16:31:32 +0100

    add work 20/03

--- Javascript/TP1 JS/EX3/script.js
+++ Javascript/TP1 JS/EX3/script.js
@@ -1,10 +1,25 @@
-let nb1 = parseInt(prompt("Quelle table ?"))
-
-document.write(<table>);
-document.write(<tr>);
+let nb1 = prompt("Quelle table ?");
 
 
+while (isNaN(nb1)){
+   nb1 = prompt("Un nombre j'ai dit !")
+}
 
-document.write(</tr>);
+for (cpt2 = 1 ; cpt2 < nb1 ; cpt2++){
+    
+}
 
-document.write(</table>);
+
+document.write("<table>");
+
+for (cpt1 = 1 ; cpt1 <= 10 ; cpt1++){
+
+document.write("<tr>");
+document.write("<td>" + nb1 + "</td>")
+document.write("<td>" + " x "+ cpt1 + "</td>")
+document.write("<td>" + " = " + (nb1*cpt1) + "</td>")
+document.write("</tr>");
+}
+
+
+document.write("</table>");
