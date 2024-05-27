32ed4ca02194459fbfe76bb76e0a3644781817db
Damien Tisné <damien.tisne@free.fr>
2024-05-27 16:44:20 +0200

    add work 27/05

---
+++ PHP/test.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    $voitures = 3;
+    $motos = 6;
+    
+    echo "J'ai $voitures voitures et $motos motos, j'ai donc " . $voitures + $motos . " vehicules. <br>"; 
+    echo 'J\'ai ' . $voitures . ' voitures et ' . $motos . ' motos, j\'ai donc ' . $voitures + $motos . ' vehicules.'; 
+    ?>
+</body>
+</html>
